acf9bdc9799fd705212584c615def300059d1d77
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-16 23:14:59 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.java
+++ math/powx-n.java
@@ -1,18 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
+        double num = n;
         double ans = 1;
-        if(n<0){
+        if(num<0){
             x = 1/x;
-            n = -n;
+            num = -num;
         }
-        while(n>0){
-            if(n%2==1){
+        while(num>0){
+            if(num%2==1){
                 ans = ans*x;
-                n--;
+                num--;
             }
             else{
                 x = x*x;
-                n = n/2;
+                num = num/2;
             }
         }
         return ans;
