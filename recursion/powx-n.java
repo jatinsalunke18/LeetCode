a20065702a172949e76ead4033478899b9151a40
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-16 23:13:40 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

---
+++ recursion/powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        if(n<0){
+            x = 1/x;
+            n = -n;
+        }
+        while(n>0){
+            if(n%2==1){
+                ans = ans*x;
+                n--;
+            }
+            else{
+                x = x*x;
+                n = n/2;
+            }
+        }
+        return ans;
+    }
+}
