a50b311065aff56b6e3ca99e23807f49ecf210f0
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-18 11:33:52 +0530

    LC-minimum-speed-to-arrive-on-time [Runtime: N/A; Memory: N/A]

--- binary-search/minimum-speed-to-arrive-on-time.java
+++ binary-search/minimum-speed-to-arrive-on-time.java
@@ -1,24 +1,25 @@
 class Solution {
-    int check(int[] nums,double mid){
+    double check(int[] nums,double mid){
         double ans = 0;
-        for(int num:nums){
-            ans+=num/mid;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i != nums.length - 1) {
+                ans += Math.ceil((double) nums[i] / mid);
+            } else {
+                ans += (double) nums[i] / mid;
+            }
         }
-        return (int)Math.ceil(ans);
+        return ans;
     }
     public int minSpeedOnTime(int[] dist, double hour) {
-        double low = 0;
-        double high =0;
-        for(int i=0;i<dist.length;i++){
-            high = Math.max(high,dist[i]);
-        }
-        if(Math.ceil(hour)<high) return -1;
+        double low = 1;
+        double high =Integer.MAX_VALUE;
+        if(hour<=dist.length-1) return -1;
         while(low<=high){
             double mid = low+(high-low)/2;
-            if(check(dist,mid)==hour) return (int)Math.ceil(mid);
-            if(check(dist,mid)<hour) high = mid-1;
+            if(check(dist,mid)<=hour) high = mid-1;
             else low = mid+1;
         }
-        return (int)Math.ceil(low)-1;
+        return (int)Math.ceil(low);
     }
 }
