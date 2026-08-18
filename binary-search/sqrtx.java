ea50efc368ffb48674d3bb2fae8cd9acac1b065f
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-18 08:29:00 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- binary-search/sqrtx.java
+++ binary-search/sqrtx.java
@@ -1,12 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        long low = 0;
-        long high = x;
+        int low = 1;
+        int high = x;
         while(low<=high){
-            long mid = low+(high-low)/2;
-            if((long)mid*mid<=x) low = mid+1;
+            int mid = low+(high-low)/2;
+            if(mid*mid==x) return mid;
+            if(mid*mid<x) low = mid+1;
             else high = mid-1;
         }
-        return (int)low-1;
+        return high;
     }
 }
