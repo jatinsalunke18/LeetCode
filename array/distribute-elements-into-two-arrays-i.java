2d3753d8919826dcadc24b99e8b3f4a6a6cc045d
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-20 22:21:59 +0530

    LC-distribute-elements-into-two-arrays-i [Runtime: 1 ms; Memory: 46.4 MB]

--- array/distribute-elements-into-two-arrays-i.java
+++ array/distribute-elements-into-two-arrays-i.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[] resultArray(int[] nums) {
         int ans[] = new int[nums.length];
-        int i=0;
-        int j=(nums.length+1)/2;
-        int k = 0;
-        ans[i++] = nums[k++];
-        ans[j++] = nums[k++];
-        while(k<nums.length && i<(nums.length+1)/2 && j<nums.length){
-            if(ans[k-2]>ans[k-1]) ans[i++] =  nums[k++];
-            else ans[j++] = nums[k++];
+        int arr1[] = new int[nums.length];
+        int arr2[] = new int[nums.length];
+        int i = 0;
+        int j = 0;
+        arr1[i++] = nums[0];
+        arr2[j++] = nums[1];
+        for(int k=2;k<nums.length;k++){
+            if(arr1[i-1]>arr2[j-1]) arr1[i++] = nums[k];
+            else arr2[j++] = nums[k];
         }
-        while(k<nums.length && i<(nums.length+1)/2) ans[i++] = nums[k++];
-        while(k<nums.length && j<nums.length) ans[j++] = nums[k++];
+        for(int k=0;k<i;k++){
+            ans[k] = arr1[k];
+        }
+        for(int k=0;k<j;k++){
+            ans[i+k] = arr2[k];
+        }
         return ans;
     }
 }
