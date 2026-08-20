64b9e60c26e6ecb1457d693a23589b6175d4c3de
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-20 09:49:28 +0530

    LC-maximum-width-ramp [Runtime: N/A; Memory: N/A]

--- stack/maximum-width-ramp.java
+++ stack/maximum-width-ramp.java
@@ -2,13 +2,17 @@ class Solution {
     public int maxWidthRamp(int[] nums) {
         int max = 0;
         int i = 0;
-        int j = nums.length-1;
-        while(i<nums.length){
-            if(i<nums.length-1 && nums[i]==nums[i+1]) max = Math.max(1,max);
+        int j = 1;
+        int k = nums.length-1;
+        while(j<nums.length){
+            if(j<nums.length-1 && nums[j]==nums[j+1]) max = Math.max(1,max);
             if(nums[i]<=nums[j]){
                 max = Math.max(max,j-i);
+                if(nums[i]<=nums[k])max = Math.max(max,k-i);
+                j++;
             }
-            i++;
+            else if(nums[i]>nums[k])i++;
+            else j++;
         }
         return max;
     }
