722127b07daf63c612e22a4f87f334d35c025959
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-20 09:01:58 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.java
+++ array/3sum.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length-2;i++){
             if(i>0&& nums[i]==nums[i-1]) continue;
             int j = i+1;
             int k = nums.length-1;
@@ -14,8 +14,8 @@ class Solution {
                     ans.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k])));
                     j++;
                     k--;
-                    while(j<k && nums[j]==nums[j+1])j++;
-                    while(j<k && nums[k]==nums[k-1])k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
             }
         }
