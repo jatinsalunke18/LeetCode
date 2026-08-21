8a3823d39f76a0257f5f65453381a4d66ea0bb24
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-21 17:44:37 +0530

    LC-remove-outermost-parentheses [Runtime: N/A; Memory: N/A]

--- stack/remove-outermost-parentheses.java
+++ stack/remove-outermost-parentheses.java
@@ -1,22 +1,20 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         int open = 1;
-        StringBuilder sb = new StringBuilder("");
-        for(int i = 1;i<s.length();i++){
-            if(s.charAt(i) == '('){
+        StringBuilder ans = new StringBuilder();
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)=='('){
                 open++;
-                if(open != 1){
-                    sb.append(s.charAt(i));
-                }
-            }else{
-                if(open == 1){
+                if(open!=1) ans.append(s.charAt(i));
+            }
+            else{
+                if(open==1) open--;
+                else{
+                    ans.append(s.charAt(i));
                     open--;
-                }else{
-                    open--;
-                    sb.append(s.charAt(i));
                 }
             }
         }
-        return sb.toString();
+        return ans.toString();
     }
 }
