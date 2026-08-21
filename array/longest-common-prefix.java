74411f49ef1c17488426af707976d11a10d18ca2
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-21 20:49:15 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- array/longest-common-prefix.java
+++ array/longest-common-prefix.java
@@ -1,10 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        if(strs[0].length()==0) return "";
         Arrays.sort(strs);
         String word1 = strs[0];
         String word2 = strs[strs.length-1];
         int i=0;
-        while(word1.charAt(i)==word2.charAt(i)){
+        while(i<=word1.length() && word1.charAt(i)==word2.charAt(i)){
             i++;
         }
         return strs[0].substring(0,i);
