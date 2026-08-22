72391dc3a9b7f4099dadb70aee606cf5726b51a3
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-22 11:17:21 +0530

    LC-minimum-length-of-string-after-deleting-similar-ends [Runtime: N/A; Memory: N/A]

--- two-pointers/minimum-length-of-string-after-deleting-similar-ends.java
+++ two-pointers/minimum-length-of-string-after-deleting-similar-ends.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumLength(String s) {
         int left = 0;
         int right = s.length()-1;
-        while(left<=right){
+        while(left<right){
             if(s.charAt(left)!=s.charAt(right)) break;
             while(s.charAt(left)==s.charAt(left+1)) left++;
             while(s.charAt(right-1)==s.charAt(right)) right--;
