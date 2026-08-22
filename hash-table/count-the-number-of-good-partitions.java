3f9a164f3eab2d13ceeca4afa4079cde40c29007
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-22 09:23:48 +0530

    LC-count-the-number-of-good-partitions [Runtime: N/A; Memory: N/A]

---
+++ hash-table/count-the-number-of-good-partitions.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfGoodPartitions(int[] nums) {
+        int rep = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>1) rep++;
+        }
+        if(map.size()==nums.length) return map.size()*2;
+        if(map.size()==rep) return map.size();
+        return map.size()-rep;
+    }
+}
