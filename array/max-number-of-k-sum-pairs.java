497ae3c79b4f9f386e3529882ed183b6a863874c
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-22 08:51:50 +0530

    LC-max-number-of-k-sum-pairs [Runtime: N/A; Memory: N/A]

--- array/max-number-of-k-sum-pairs.java
+++ array/max-number-of-k-sum-pairs.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maxOperations(int[] arr, int k) {
         int cnt = 0;
-        ArrayList<Integer> set = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<arr.length;i++){
             int rem = k - arr[i];
-            if(set.contains(rem)){
-                cnt++;
-                set.remove(Integer.valueOf(rem));
+            if(map.containsKey(rem)){
+                    cnt++;
+                    map.put(rem,map.get(rem)-1);
+                    if(map.get(rem)==0) map.remove(rem);
             }
-            else set.add(arr[i]);
+            else map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
         return cnt;
     }
