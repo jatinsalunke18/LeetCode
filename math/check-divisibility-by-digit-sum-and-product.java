127485b45462891a12059fbdaf7cf48e4ae6cf84
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-22 07:57:00 +0530

    LC-check-divisibility-by-digit-sum-and-product [Runtime: N/A; Memory: N/A]

---
+++ math/check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int pro = 1;
+        int sum = 0;
+        int num = n;
+        while(num>0){
+            int dig = num%10;
+            pro *= dig;
+            sum += dig;
+            num = num/10;
+        }
+        if(pro == 0)return false;
+        if(n%(pro+sum)==0) return true;
+        return false;
+    }
+}
