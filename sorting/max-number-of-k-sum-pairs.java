437c84f69ad5e5816b71f06ac950c8a0b8595063
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-22 08:42:53 +0530

    LC-max-number-of-k-sum-pairs [Runtime: N/A; Memory: N/A]

--- sorting/max-number-of-k-sum-pairs.java
+++ sorting/max-number-of-k-sum-pairs.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxOperations(int[] arr, int k) {
         int cnt = 0;
-        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> set = new ArrayList<>();
         for(int i=0;i<arr.length;i++){
             int rem = k - arr[i];
             if(set.contains(rem)){
                 cnt++;
-                set.remove(rem);
+                set.remove(Integer.valueOf(rem));
             }
             else set.add(arr[i]);
         }
