93ae992a37dd9a731ed1ea511877cf19d78ae0a2
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-26 13:53:15 +0530

    LC-fruit-into-baskets [Runtime: N/A; Memory: N/A]

--- array/fruit-into-baskets.java
+++ array/fruit-into-baskets.java
@@ -2,11 +2,20 @@ class Solution {
     public int totalFruit(int[] fruits) {
         HashMap<Integer,Integer> map = new HashMap<>();
         int max = 0;
+        int k=0;
         for(int i=0;i<fruits.length;i++){
             int temp = 0;
-            if(map.size()==2){
-                
+            map.put(fruits[i],map.getOrDefault(fruits[i],0)+1);
+            if(map.size()<=2){
+                for(int key:map.keySet()){
+                    temp+=map.get(key);
+                }
+                max = Math.max(max,temp);
+            }
+            if(map.size()>2){
+                map.remove(fruits[k++]);
             }
         }
+        return max;
     }
-    }
+}
