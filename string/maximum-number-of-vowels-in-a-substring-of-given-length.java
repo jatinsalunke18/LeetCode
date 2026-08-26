13998abcc8e983a627063f900255d13652c05e84
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-26 10:55:42 +0530

    LC-maximum-number-of-vowels-in-a-substring-of-given-length [Runtime: N/A; Memory: N/A]

--- string/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ string/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,11 +1,15 @@
 class Solution {
     public int maxVowels(String s, int k) {
         int max = 0;
-        for(int i=0;i<s.length()-k;i++){
-            int cnt = 0;
-            for(int j=i;j<i+k;j++){
-                if(s.charAt(j)=='a'||s.charAt(j)=='e'||s.charAt(j)=='i'||s.charAt(j)=='o'||s.charAt(i)=='u') cnt++;
-            }
+        for(int i=0;i<k;i++){
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='u'||s.charAt(i)=='o') max++;
+        }
+        int cnt = max;
+
+        
+        for(int i=k;i<s.length();i++){
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='u'||s.charAt(i)=='o') cnt++;
+            if(s.charAt(i-k)=='a'||s.charAt(i-k)=='e'||s.charAt(i-k)=='i'||s.charAt(i-k)=='u'||s.charAt(i-k)=='o') cnt--;
             max = Math.max(max,cnt);
         }
         return max;
