f88347edb3bc964633d2a3efa37fbcfe6fac9815
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-26 13:38:55 +0530

    LC-fruit-into-baskets [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/fruit-into-baskets.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = 0;
+        for(int i=0;i<fruits.length;i++){
+            int temp = 0;
+            if(map.size()==2){
+                
+            }
+        }
+    }
+    }
