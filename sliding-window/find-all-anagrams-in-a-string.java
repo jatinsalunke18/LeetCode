77013e8ba3083bd2ad6f2509ed9f7e2019ecc9af
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-26 13:16:06 +0530

    LC-find-all-anagrams-in-a-string [Runtime: N/A; Memory: N/A]

--- sliding-window/find-all-anagrams-in-a-string.java
+++ sliding-window/find-all-anagrams-in-a-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
+        if(p.length()>s.length()) return ans;
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
         for(int i=0;i<p.length();i++){
