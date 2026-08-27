81d65b84d09d08bb8812891c559d4125c4296b6c
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-27 10:56:18 +0530

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- prefix-sum/minimum-size-subarray-sum.java
+++ prefix-sum/minimum-size-subarray-sum.java
@@ -5,13 +5,14 @@ class Solution {
         int sum = 0;
         for(int i=0;i<nums.length;i++){
             sum += nums[i];
-            while(sum>target){
+            while(sum>=target){
+                len = Math.min(len,i-k+1);
                 sum -= nums[k];
                 k++;
             }
-            if(sum==target){
-                len = Math.min(len,i-k+1);
-            }
+            // if(sum==target){
+            //     len = Math.min(len,i-k+1);
+            // }
         }
         if(len == Integer.MAX_VALUE) return 0;
         return len;
