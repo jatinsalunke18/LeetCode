df1a1adb72394e43d7fb2ae960ff0f82a3e6c6f4
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-27 11:30:24 +0530

    LC-binary-subarrays-with-sum [Runtime: N/A; Memory: N/A]

--- prefix-sum/binary-subarrays-with-sum.java
+++ prefix-sum/binary-subarrays-with-sum.java
@@ -1,23 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int count = 0;
-        int last = 0;
-        int j =  0;
+        int j = 0;
         int sum = 0;
-        for(int  i=0;i<nums.length;i++){
-            sum+=nums[i];
-        }
-        if(sum==goal) return (nums.length*(nums.length+1))/2;
-        sum = 0;
         for(int i=0;i<nums.length;i++){
-            sum += nums[i];
-            while(j<i && sum>goal){
-                sum -= nums[j];
+            sum+=nums[i];
+            while(sum>goal){
+                sum-=nums[j];
                 j++;
             }
             if(sum==goal){
-                count+= i-last;
-                last = i;
+                int k = j;
+                while(k<=i && nums[k]==0){
+                    count++;
+                    k++;
+                }
+                if(k<i && nums[k]==1) count++;
             }
         }
         return count;
