ffbd6116a7a9f010141c639ab280c1bfcf3b24ec
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-09-05 17:16:12 +0530

    LC-append-characters-to-string-to-make-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/append-characters-to-string-to-make-subsequence.java
+++ greedy/append-characters-to-string-to-make-subsequence.java
@@ -1,5 +1,6 @@
 class Solution {
     public int appendCharacters(String s, String t) {
+        if(s.contains(t)) return 0;
         int i=0;
         while(i<t.length() && s.charAt(i)==t.charAt(i)) i++;
         return t.length()-i;
