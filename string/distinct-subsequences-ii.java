423ba3995a9283a79711db88f3be19311d5cac02
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-09-07 07:34:02 +0530

    LC-distinct-subsequences-ii [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences-ii.java
+++ string/distinct-subsequences-ii.java
@@ -1,19 +1,14 @@
 class Solution {
     public int mod = (int) 1e9+7;
-    void func(int ind,String s,StringBuilder temp,HashSet<String> ans){
-        if(ind==s.length()){
-            if(temp.length()!=0) ans.add(temp.toString());
-            return;
-        }
-        temp.append(s.charAt(ind));
-        func(ind+1,s,temp,ans);
-        temp.deleteCharAt(temp.length()-1);
-        func(ind+1,s,temp,ans);
-    }
     public int distinctSubseqII(String s) {
-        HashSet<String> ans = new HashSet<>();
-        StringBuilder temp = new StringBuilder();
-        func(0,s,temp,ans);
-        return ans.size()%mod;
+        long[] last = new long[26];
+        long total = 0;
+        for(char c:s.toCharArray()){
+
+            long newsubsequences = (total+1)%mod;
+            total = (total + (total+1) - last[c-'a'] + mod)%mod;
+            last[c-'a'] = newsubsequences;
+        }
+        return (int) total;
     }
 }
